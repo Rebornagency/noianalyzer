67e530c483c99772679f6ed322232c5df46a6e39
Rebornagency <edgaralvarezllc@gmail.com>
2025-05-03 19:19:43 -0400

    Add files via upload

--- noi_tool_batch_integration.py
+++ noi_tool_batch_integration.py
@@ -8,7 +8,7 @@ from noi_calculations import calculate_noi_comparisons
 from ai_insights_gpt import generate_insights_with_gpt
 from insights_display import display_insights
 from fixed_ai_extraction import extract_noi_data
-from utils.helpers import format_for_noi_comparison
+from utils.helpers import format_for_noi_comparison, determine_document_type
 
 # Configure logging
 logging.basicConfig(
