40792451e3243557f4198b31a937707d9730af72
Rebornagency <edgaralvarezllc@gmail.com>
2025-07-07 19:21:07 -0400

    Update api_server_minimal.py

--- api_server_minimal.py
+++ api_server_minimal.py
@@ -333,6 +333,6 @@ if __name__ == "__main__":
     uvicorn.run(
         "api_server_minimal:app",
         host="0.0.0.0",
-        port=8000,
-        reload=True
+        port=int(os.environ.get("PORT", 8000)),
+        reload=False
     ) 
